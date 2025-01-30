0a0088afad278d1ccef0895c0324d21b33cb35df
Nishanth985589 <nishanthraina003@gmail.com>
2025-01-30 23:23:18 +0530

    windows completed

---
+++ task9/src/main/java/task9/task9/App.java
@@ -0,0 +1,13 @@
+package task9.task9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
